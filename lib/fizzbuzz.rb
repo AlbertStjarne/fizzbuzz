c325dc9d7bb1283864b68bf84eb6a5f5c8c9d889
Albert Stjärne <albert.stjarne@gmail.com>
2018-08-14 13:12:48 +0200

    first commit on exercise fizzbuzz

---
+++ lib/fizzbuzz.rb
@@ -0,0 +1,20 @@
+
+def fizzbuzz(number)
+   
+    if has_zero_remainder?(number, 15)
+        'fizzbuzz'
+    elsif has_zero_remainder?(number, 3)
+        'fizz'
+    elsif has_zero_remainder?(number, 5)
+        'buzz'
+    else
+        number
+    end
+end
+
+
+def has_zero_remainder?(number, divider)
+    number % divider == 0
+end
+
+
---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,24 @@
+require './lib/fizzbuzz'    # The tests file will look for and test the source 
+                            # file in the lib folder, called fizzbuzz.rb.
+
+describe 'fizzbuzz' do      # A describe block where we will put our tests.
+
+it 'returns 1 if number is 1' do
+        expect(fizzbuzz(1)).to eq 1
+end
+
+it "returns 'fizz' if number is divisible by 3" do
+    expect(fizzbuzz(3)).to eq 'fizz'
+
+end
+it "returns 'buzz' if number is divisible by 5" do
+    expect(fizzbuzz(5)).to eq 'buzz'
+end
+
+it "returns 'fizzbuzz' if number is divisible by 15" do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+end
+
+
+
+end
